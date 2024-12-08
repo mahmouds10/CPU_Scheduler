4952badfd73a951b598c3d432b498483ffc6e94e
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2024-12-09 00:46:08 +0200

    Update the interface

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,5 +1,8 @@
 import java.util.List;
+import java.util.Map;
 
 public interface Scheduler {
     List<Process> schedule(List<Process> processes);
+    public List<Execution> getExecutions();
+    Map<String, Double> getSchedulingSummary();
 }
