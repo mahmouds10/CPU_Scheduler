f04c1ff865740df348e0e11423350852728510e9
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2024-12-09 01:15:35 +0200

    Update main function

--- src/CPUSchedulersSimulator.java
+++ src/CPUSchedulersSimulator.java
@@ -13,10 +13,21 @@ public class CPUSchedulersSimulator {
         processes.add(new Process("P3", "#00FF00", 4, 1, 1)); // Green
         processes.add(new Process("P4", "#FFFF00", 5, 4, 1)); // Yellow
 
+        // Test for Priority
+//        processes.add(new Process("P1", "#0000FF", 0, 7, 4)); // Blue
+//        processes.add(new Process("P2", "#FF0000", 0, 4, 1)); // Red
+//        processes.add(new Process("P3", "#00FF00", 0, 1, 3)); // Green
+//        processes.add(new Process("P4", "#FFFF00", 0, 4, 2)); // Yellow
 
-        System.out.println("Select Scheduler: 1. SRTF 2. SJF 3. FCAI");
+
+        System.out.println("Select Scheduler: ");
+        System.out.println("1 ====> SRTF ");
+        System.out.println("2 ====> SJF  ");
+        System.out.println("3 ====> Priority");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
+
+        System.out.println("\n");
 
         Scheduler scheduler;
         String schedulerName;
@@ -30,6 +41,10 @@ public class CPUSchedulersSimulator {
                 scheduler = new SJFScheduler();
                 schedulerName = "SJF";
                 break;
+            case 3:
+                scheduler = new PriorityScheduler();
+                schedulerName = "Priority";
+                break;
             default:
                 System.out.println("Invalid choice.");
                 return;
@@ -37,7 +52,7 @@ public class CPUSchedulersSimulator {
 
         scheduler.schedule(new ArrayList<>(processes));
         Map<String, Double> summary = scheduler.getSchedulingSummary();
-        List <Execution> executionsSequence = scheduler.getExecutions();
+        List<Execution> executionsSequence = scheduler.getExecutions();
         new SchedulerGUI(
                 schedulerName,
                 executionsSequence,
