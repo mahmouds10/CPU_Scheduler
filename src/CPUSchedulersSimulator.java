c78504b1ad5c71554fec6711e856723055af78ae
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2024-12-09 23:06:05 +0200

    Update main function

--- src/CPUSchedulersSimulator.java
+++ src/CPUSchedulersSimulator.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.*;
 
 
@@ -13,6 +12,16 @@ public class CPUSchedulersSimulator {
         processes.add(new Process("P2", "#FF0000", 2, 4, 1)); // Red
         processes.add(new Process("P3", "#00FF00", 4, 1, 1)); // Green
         processes.add(new Process("P4", "#FFFF00", 5, 4, 1)); // Yellow
+
+
+        Queue<ProcessThread> queue = new LinkedList<>();
+        List<ProcessThread> FCAIprocesses = Arrays.asList(
+                new ProcessThread("P1", "Red", 0, 17, 4, 4, queue),
+                new ProcessThread("P2", "Blue", 3, 6, 9, 3, queue),
+                new ProcessThread("P3", "Green", 4, 10, 3, 5, queue),
+                new ProcessThread("P4", "Yellow", 29, 4, 10, 2, queue)
+        );
+
 
         // Test for Priority
 //        processes.add(new Process("P1", "#0000FF", 0, 7, 4)); // Blue
@@ -34,38 +43,40 @@ public class CPUSchedulersSimulator {
         Scheduler scheduler;
         String schedulerName;
 
-        switch (choice) {
-            case 1:
-                scheduler = new SRTFScheduler();
-                schedulerName = "SRTF";
-                break;
-            case 2:
-                scheduler = new SJFScheduler();
-                schedulerName = "SJF";
-                break;
-            case 3:
-                scheduler = new PriorityScheduler();
-                schedulerName = "Priority";
-                break;
-            case 4:
-                scheduler = new FCAIScheduler();
-                schedulerName = "FCAI";
-                break;
-            default:
-                System.out.println("Invalid choice.");
-                return;
+        if (choice == 1 || choice == 2 || choice == 3) {
+            switch (choice) {
+                case 1:
+                    scheduler = new SRTFScheduler();
+                    schedulerName = "SRTF";
+                    break;
+                case 2:
+                    scheduler = new SJFScheduler();
+                    schedulerName = "SJF";
+                    break;
+                case 3:
+                    scheduler = new PriorityScheduler();
+                    schedulerName = "Priority";
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    return;
+            }
+
+            scheduler.schedule(new ArrayList<>(processes));
+            Map<String, Double> summary = scheduler.getSchedulingSummary();
+            List<Execution> executionsSequence = scheduler.getExecutions();
+            new SchedulerGUI(
+                    schedulerName,
+                    executionsSequence,
+                    summary.get("Average Waiting Time"),
+                    summary.get("Average Turnaround Time")
+            );
+        } else if (choice == 4) {
+            FCAIScheduler FcaiScheduler = new FCAIScheduler();
+            FcaiScheduler.schedule(FCAIprocesses, queue);
+        } else {
+            System.out.println("Invalid choice.");
         }
-
-        scheduler.schedule(new ArrayList<>(processes));
-        Map<String, Double> summary = scheduler.getSchedulingSummary();
-        List<Execution> executionsSequence = scheduler.getExecutions();
-        new SchedulerGUI(
-                schedulerName,
-                executionsSequence,
-                summary.get("Average Waiting Time"),
-                summary.get("Average Turnaround Time")
-        );
-
     }
 
 }
